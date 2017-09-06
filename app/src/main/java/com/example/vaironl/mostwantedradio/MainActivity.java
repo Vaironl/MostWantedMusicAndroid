e895636fd82fc9420581c9a99988386ac31dc18a
vaironl <vairon@live.com>
2017-09-05 22:37:09 -0400

    Minor changes to get an access token which did not work.

--- app/src/main/java/com/example/vaironl/mostwantedradio/MainActivity.java
+++ app/src/main/java/com/example/vaironl/mostwantedradio/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.vaironl.mostwantedradio;
+
 import com.facebook.AccessToken;
+import com.facebook.AccessTokenSource;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
@@ -8,10 +10,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,32 +48,52 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
-    private void init(){
+    private void init() {
         imageTL = (TableLayout) findViewById(R.id.imagesTableLayout);
+        getImages();
 
     }
 
-    private void getImages(){
+    private void getImages() {
+        String ACCESSTOKEN = "EAACEdEose0cBAExzgEKkE7sCmST8IDZAxf3XIDOnfNjfCjUIkvVGY7aefmrqWZA76fZA94ZBa5VsjvQRLeTgRIckn4Y48G2pMTFZCQD0Ea6ZCNvGlzZANDdk3fSE2ZBcVpl9we14O4ke17EAjD0OcgfULwc38W7NO6TuEZAScQpaZCiBfqNIvWmbC9j8HL8Ijz42AqvxZB181wyyQZDZD";
         AccessToken accessToken = null;
 
-        GraphRequest request = GraphRequest.newGraphPathRequest(null, "/GameOfThrones/photos",
-                new GraphRequest.Callback(){
+        GraphRequest request = GraphRequest.newMeRequest(
+                accessToken,
+                new GraphRequest.GraphJSONObjectCallback() {
                     @Override
-                    public void onCompleted(GraphResponse response) {
-                        //Insert your code here
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+                        // Application code
+                        if (response != null) {
+                            Log.i("GraphAPIInfo", "Response was null");
+                            Log.i("GraphAPIInfo", isObjectNull(object));
+                        } else {
+                            Log.i("GraphAPIInfo", response.getRawResponse());
+
+                        }
+
                     }
                 });
-
         Bundle parameters = new Bundle();
-        parameters.putString("fields", "picture");
+        parameters.putString("fields", "id,name,link");
         request.setParameters(parameters);
         request.executeAsync();
     }
 
-    private void hideImages(){
+    private String isObjectNull(Object object){
+        if(object != null){
+            return "It is not a null object";
+        }
+        return "It is a null object";
+    }
+
+    private void hideImages() {
         imageTL.setVisibility(View.GONE);
     }
-    private void showImages(){
+
+    private void showImages() {
         imageTL.setVisibility(View.VISIBLE);
     }
 
